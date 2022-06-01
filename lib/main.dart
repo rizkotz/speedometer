5a93c6cf79333398d9bdece524ef1d9608541547
NaufaAlfi <naufalalfi100@gmail.com>
2022-06-02 02:00:06 +0700

    Test3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Speedometer',
+      title: 'SPEEDOMETER',
       theme: ThemeData(
         // This is the theme of your application.
         //

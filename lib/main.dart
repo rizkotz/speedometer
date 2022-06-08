e8b787a2cbf6addf5a09f5d5d518c4cb0399752c
rizkotz <rizqifauzan28@gmail.com>
2022-06-09 02:04:44 +0700

    speedo

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:speedometer/screens/dash_screen.dart';
+
+Future<void> main() async {
+  // Placeholder Splash Screen Material App.
+  runApp(const NoPermissionApp(hasCheckedPermissions: false));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied ||
+      permission == LocationPermission.unableToDetermine) {
+    permission = await GeolocatorPlatform.instance.requestPermission();
+  }
+  switch (permission) {
+    case LocationPermission.deniedForever:
+      runApp(const NoPermissionApp(hasCheckedPermissions: true));
+      break;
+
+    case LocationPermission.always:
+    case LocationPermission.whileInUse:
+      runApp(const SpeedometerApp());
+      break;
+
+    case LocationPermission.denied:
+    case LocationPermission.unableToDetermine:
+      runApp(const NoPermissionApp(hasCheckedPermissions: false));
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+/// MaterialApp that launches when proper permissions granted
+class SpeedometerApp extends StatefulWidget {
+  const SpeedometerApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _SpeedometerAppState createState() => _SpeedometerAppState();
+}
+
+class _SpeedometerAppState extends State<SpeedometerApp> {
+  /// Shared preferences to be loaded for persistence.
+  SharedPreferences? sharedPreferences;
+
+  /// Unit selection
+  final List<String> units = const <String>['m/s', 'km/h', 'miles/h'];
+  String currentSelectedUnit = 'm/s';
+
+  /// Function to save newly selected unit [newUnit] to persistent storage if possible, and update state.
+  void unitSelectorFunciton(String newUnit) {
+    if (sharedPreferences != null) {
+      sharedPreferences!.setString('unit', newUnit);
+    }
+    setState(() => currentSelectedUnit = newUnit);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Load Selected unit through Shared Preferences
+    SharedPreferences.getInstance().then(
+      (SharedPreferences prefs) {
+        sharedPreferences = prefs;
+        setState(
+          () => currentSelectedUnit = (prefs.getString('unit') ?? 'm/s'),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Voice Speedometer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            'Speedometer',
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(color: Colors.white),
+          ),
+          backgroundColor: Colors.black,
+          // Makes one Unit Selection button for each potential unit (m/s, km/h and miles/h programmed)
+          actions: units.map<Widget>(
+            (String unitType) {
+              return _UnitSelectionButton(
+                unitButtonName: unitType,
+                currentSelectedUnit: currentSelectedUnit,
+                unitSelector: unitSelectorFunciton,
+              );
+            },
+          ).toList(),
+        ),
+        body: DashScreen(unit: currentSelectedUnit),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+/// TextButton that enables user to select this particular unit
+class _UnitSelectionButton extends StatelessWidget {
+  const _UnitSelectionButton({
+    Key? key,
+    this.unitButtonName = 'm/s',
+    required this.currentSelectedUnit,
+    required this.unitSelector,
+  }) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String unitButtonName, currentSelectedUnit;
+  final void Function(String) unitSelector;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final Color textColor = unitButtonName != currentSelectedUnit
+        ? Colors.white
+        : const Color(0xFFE9A246);
+    return Container(
+      padding: const EdgeInsets.only(right: 10),
+      child: TextButton(
+        onPressed: () => unitSelector(unitButtonName),
+        style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.zero)),
+        child: Text(unitButtonName, style: TextStyle(color: textColor)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+/// MaterialApp that launches when permissions still being searched, or denied forever.
+class NoPermissionApp extends StatelessWidget {
+  const NoPermissionApp({
+    Key? key,
+    required bool hasCheckedPermissions,
+  })  : _hasCheckedPermissions = hasCheckedPermissions,
+        super(key: key);
+
+  final bool _hasCheckedPermissions;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget outWidget;
+    // Splash screen mode
+    if (!_hasCheckedPermissions) {
+      outWidget = const Image(
+        image: AssetImage('images/splash_image.png'),
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+      );
+    } else {
+      outWidget = const Text(
+        'Location permissions permanently denied!\n'
+        'Please reinstall app and provide permissions!',
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
         ),
+      );
+    }
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(child: outWidget),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
